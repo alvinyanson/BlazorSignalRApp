ed32b992dba7aa0ef2628a107e1b8125ff2c887b
alvinyanson <a.yanson@arcanys.com>
2024-08-08 11:59:04 +0800

    Add project files.

---
+++ BlazorSignalRApp/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalRApp.Hubs;
+
+public class ChatHub : Hub<IChatClient>
+{
+    public async Task SendMessage(string user, string message)
+    {
+        Console.WriteLine($"Connection Id: {Context.ConnectionId}");
+
+        await Clients.Caller.ResponseToCaller("Server message: This message is intended for the caller.");
+
+        await Clients.Others.ResponseToAllExceptCaller("\"Server message: This message is intended for all except the caller.\"");
+
+        await Clients.All.ReceiveMessage(user, message);
+    }
+}
---
+++ BlazorSignalRApp/Hubs/IChatClient.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorSignalRApp.Hubs
+{
+    public interface IChatClient
+    {
+        Task ResponseToCaller(string message);
+        Task ResponseToAllExceptCaller(string message);
+        Task ReceiveMessage(string user, string message);
+    }
+}
